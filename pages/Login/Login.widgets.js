06c831f54e6c1fded850191a22ca235ac1804ff4
alessioguidi <alessio.guidi@gmail.com>
2015-06-05 11:23:41 +0200

    Release 2.4

--- pages/Login/Login.widgets.js
+++ pages/Login/Login.widgets.js
@@ -1,9 +1,20 @@
 Login.widgets = {
+	navigationCallSettings: ["wm.NavigationCall", {"operation":"gotoPage"}, {}, {
+		input: ["wm.ServiceInput", {"type":"gotoPageInputs"}, {}, {
+			binding: ["wm.Binding", {}, {}, {
+				wire: ["wm.Wire", {"expression":"\"Configurazione\"","targetProperty":"pageName"}, {}]
+			}]
+		}]
+	}],
 	layoutBox1: ["wm.Layout", {"horizontalAlign":"center","verticalAlign":"top"}, {}, {
-		panel1: ["wm.Panel", {"enableTouchHeight":true,"height":"100%","horizontalAlign":"center","verticalAlign":"middle","width":"100%"}, {}, {
+		panel1: ["wm.Panel", {"enableTouchHeight":true,"height":"100%","horizontalAlign":"center","styles":{},"verticalAlign":"middle","width":"100%"}, {}, {
 			textUser: ["wm.Text", {"caption":"User","dataValue":"mob","desktopHeight":"35px","displayValue":"mob","height":"36px","required":true,"width":"90%"}, {}],
 			textPassword: ["wm.Text", {"caption":"Password","dataValue":undefined,"desktopHeight":"35px","displayValue":"","height":"36px","password":true,"width":"90%"}, {}],
-			buttonLogin: ["wm.Button", {"border":"0","caption":"Enter","height":"40px","width":"80px"}, {"onclick":"buttonLoginClick"}]
+			buttonLogin: ["wm.Button", {"border":"0","caption":"Enter","height":"40px","width":"80px"}, {"onclick":"buttonLoginClick"}],
+			spacer1: ["wm.Spacer", {"height":"48px","styles":{},"width":"96px"}, {}],
+			panel2: ["wm.Panel", {"height":"48px","horizontalAlign":"left","layoutKind":"left-to-right","styles":{},"verticalAlign":"top","width":"100%"}, {}, {
+				label1: ["wm.Label", {"align":"center","caption":"Settings","padding":"4","styles":{"fontSize":"10px","textDecoration":"underline"},"width":"100%"}, {"onclick":"navigationCallSettings"}]
+			}]
 		}]
 	}]
 }
